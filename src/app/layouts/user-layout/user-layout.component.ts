c1d1916712219f13ac0122fc3f1cd34d5fe38e20
GamalEl-Shamy <gamalelshamy74@gmail.com>
2026-06-26 17:29:19 +0300

    feat(user): enhance sidebar UI and implement dark mode

--- src/app/layouts/user-layout/user-layout.component.ts
+++ src/app/layouts/user-layout/user-layout.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLinkActive, RouterLinkWithHref, RouterOutlet } from '@angular/router';
 import { FooterComponent } from "../../shared/components/footer/footer.component";
+import { AuthService } from '../../modules/auth/services/auth.service';
+import { ThemeService } from '../../shared/services/theme.service';
 
 @Component({
   selector: 'app-user-layout',
@@ -8,26 +10,30 @@ import { FooterComponent } from "../../shared/components/footer/footer.component
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.css',
 })
-export class UserLayoutComponent  implements OnInit {
-  isSidebarOpen = signal(false);
-  userFirstName:string = "";
-  userRole:string  = "";
-  char:string  = "";
+export class UserLayoutComponent implements OnInit {
+  themeService = inject(ThemeService);
+  authService = inject(AuthService);
 
+  isSidebarOpen = signal(true); //false
+  userFirstName: string = '';
+  userRole: string = '';
+  char: string = '';
 
   ngOnInit(): void {
     if (typeof window != 'undefined') {
       this.userFirstName = localStorage.getItem('userFirstNameZaraa')!;
       this.userRole = localStorage.getItem('userRoleZaraa')!;
-      this.char = this.userFirstName[0]!.toUpperCase();
+      if (this.userFirstName && this.userFirstName.length > 0) {
+        this.char = this.userFirstName[0]!.toUpperCase();
+      }
     }
   }
 
   toggleSidebar() {
-    this.isSidebarOpen.update(state => !state);
+    this.isSidebarOpen.update((state) => !state);
   }
 
   closeSidebar() {
-    this.isSidebarOpen.set(false);
+    this.isSidebarOpen.set(true);
   }
 }
