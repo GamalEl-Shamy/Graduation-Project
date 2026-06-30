fe4d6eff48711eb63cfcc8f7c8c34862cdbba092
GamalEl-Shamy <gamalelshamy74@gmail.com>
2026-06-30 19:52:16 +0300

    refactor(user-diagnosis): smooth out entry animations and fix duration controls

--- src/app/modules/user/pages/diagnosis/diagnosis.component.ts
+++ src/app/modules/user/pages/diagnosis/diagnosis.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, signal, viewChild } from '@angular/core';
 import { DiagnosisSectionComponent } from "../../components/diagnosis/diagnosis-section/diagnosis-section.component";
 import { DiagnosisHeaderComponent } from "../../components/diagnosis/diagnosis-header/diagnosis-header.component";
 
@@ -10,5 +10,26 @@ import { DiagnosisHeaderComponent } from "../../components/diagnosis/diagnosis-h
 })
 export class DiagnosisComponent {
 
+  secondCompContainer = viewChild<ElementRef<HTMLDivElement>>('secondCompWrapper');
+  
+  isSecondCompVisible = signal<boolean>(false);
+
+  ngAfterViewInit() {
+    const element = this.secondCompContainer()?.nativeElement;
+    
+    if (element) {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            this.isSecondCompVisible.set(true);
+            observer.unobserve(element);
+          }
+        },
+        { threshold: 0.1 }
+      );
+
+      observer.observe(element);
+    }
+  }
   
 }
