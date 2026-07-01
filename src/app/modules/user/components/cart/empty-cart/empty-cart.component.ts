b458284d9080d208eb62fafb7ff0d07070cb46e3
GamalEl-Shamy <gamalelshamy74@gmail.com>
2026-07-02 02:05:02 +0300

    feat(user-empty-cart): integrate appSlideIn directive for scroll animations

--- src/app/modules/user/components/cart/empty-cart/empty-cart.component.ts
+++ src/app/modules/user/components/cart/empty-cart/empty-cart.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { SlideIn } from "../../../../../shared/directives/slide-in";
 
 @Component({
   selector: 'app-empty-cart',
-  imports: [RouterLink],
+  imports: [RouterLink, SlideIn],
   templateUrl: './empty-cart.component.html',
   styleUrl: './empty-cart.component.css',
 })
