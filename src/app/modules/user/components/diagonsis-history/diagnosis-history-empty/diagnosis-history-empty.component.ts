900f9b31b54f5702ca4eb895cdbe804cda20f978
GamalEl-Shamy <gamalelshamy74@gmail.com>
2026-07-02 03:11:39 +0300

    feat(user-diagnosis-history): integrate appSlideIn directive for scroll animations

--- src/app/modules/user/components/diagonsis-history/diagnosis-history-empty/diagnosis-history-empty.component.ts
+++ src/app/modules/user/components/diagonsis-history/diagnosis-history-empty/diagnosis-history-empty.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { SlideIn } from "../../../../../shared/directives/slide-in";
 
 @Component({
   selector: 'app-diagnosis-history-empty',
-  imports: [RouterLink],
+  imports: [RouterLink, SlideIn],
   templateUrl: './diagnosis-history-empty.component.html',
   styleUrl: './diagnosis-history-empty.component.css',
 })
