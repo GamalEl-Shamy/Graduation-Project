f16dd4231d0ce82a200d9fa4aa5310300ccdbe78
GamalEl-Shamy <gamalelshamy74@gmail.com>
2026-07-08 01:17:55 +0300

    fix: resolve prerendering build error for diagnosis details route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
+    path: 'users/diagnosis-details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
