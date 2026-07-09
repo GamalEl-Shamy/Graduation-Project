e4b6af2343ad3b685382b2d71fb9264998877c97
GamalEl-Shamy <gamalelshamy74@gmail.com>
2026-07-09 03:02:02 +0300

    feat: enhance footer UI and implement dark mode

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,20 +1,47 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, inject, input, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {
 
+  private http = inject(HttpClient);
+  private formspreeUrl = 'https://formspree.io/f/xjgqqyrb';
+
+  contactEmail = signal<string>('');
+  contactMessage = signal<string>('');
+  isSending = signal<boolean>(false);
   currentYear = new Date().getFullYear();
-  
-  quickLinks = [
-    { name: 'Home', path: '/users/dashboard' },
-    { name: 'Diagnosis', path: '/users/diagnosis' },
-    { name: 'My Garden', path: '/users/my-garden' },
-    { name: 'Shop', path: '/users/shop' }
-  ];
+  isVisitor = input<boolean>(false);
+
+  sendMessage() {
+    if (!this.contactEmail() || !this.contactMessage()) return;
+    this.isSending.set(true);
+    const payload = {
+      email: this.contactEmail(),
+      message: this.contactMessage()
+    };
+
+    const headers = new HttpHeaders({
+      'Accept': 'application/json'
+    });
+
+    this.http.post(this.formspreeUrl, payload, { headers }).subscribe({
+      next: () => {
+        this.isSending.set(false);
+        this.contactEmail.set('');
+        this.contactMessage.set('');
+      },
+      error: (err) => {
+        this.isSending.set(false);
+        console.error('Error sending message:', err);
+      }
+    });
+  }
 }
